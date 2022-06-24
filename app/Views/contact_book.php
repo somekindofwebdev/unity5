87de5cf2462c4a4bae40ccacc59311ec5968c855
Richard Wortham <richard@somekindofweb.dev>
2022-06-24 17:24:09 +0100

    Start Vue frontend

--- app/Views/contact_book.php
+++ app/Views/contact_book.php
@@ -1,15 +1,33 @@
 <script src="https://unpkg.com/vue@3"></script>
 
-<div id="app">{{ message }}</div>
-
-<script>
+<script type="module">
   const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
+createApp({
+  data() {
+    return {
+      todoId: 1,
+      todoData: null
     }
-  }).mount('#app')
+  },
+  methods: {
+    async fetchData() {
+      this.todoData = null
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${this.todoId}`
+      )
+      this.todoData = await res.json()
+    }
+  },
+  mounted() {
+    this.fetchData()
+  }
+}).mount('#app')
 </script>
+
+<div id="app">
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
+</div>
