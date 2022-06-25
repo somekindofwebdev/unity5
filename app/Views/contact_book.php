bc7fd63020714e4dffdf7fdd57619d2c82ffc0b8
Richard Wortham <richard@somekindofweb.dev>
2022-06-25 08:32:26 +0100

    Editor two-way binding

--- app/Views/contact_book.php
+++ app/Views/contact_book.php
@@ -6,13 +6,19 @@
     const { createApp } = Vue
     import Contact from "./js/contact.js"
     createApp({
-      data() {
-        return {
-          contactId: 1,
-          contactData: null
-        }
-      },
-      methods: {
+        data() {
+            return {
+                contactId: 1,
+                contactData: null,
+                contact: {}
+            }
+        },
+        watch: {
+            contactId(newId) {
+                this.contact = this.contactData[this.contactId];
+            }
+        },
+        methods: {
         async fetchData() {
           this.contactData = null
           fetch(
@@ -27,25 +33,32 @@
           .then(response => response.json())
           .then(data => this.contactData = data)
         }
-      },
-    components: {
-        Contact
-    },
-      mounted() {
-        this.fetchData()
-      }
-  }).mount('#app')
+        },
+        components: {
+            Contact
+        },
+        mounted() {
+            this.fetchData()
+        }
+    }).mount('#app')
 </script>
 
 <div id="app">
-    <ul>
-        <contact
-                 v-for="c in contactData"
-                 :first_name="c.first_name"
-                 :middle_name="c.middle_name"
-                 :last_name="c.last_name"
-                 :address="c.address"
-                 :postcode="c.postcode"
-                 ></contact>
-    </ul>
+    <div id=contact-list>
+        <ul>
+            <contact
+                     v-for="c in contactData"
+                     :first_name="c.first_name"
+                     :middle_name="c.middle_name"
+                     :last_name="c.last_name"
+                     :address="c.address"
+                     :postcode="c.postcode"
+                     @click="this.contactId = c.ID - 1"
+                     ></contact>
+        </ul>
+    </div>
+    <div id=contact-editor>
+        <label for="editor-firstname">First name:</label>
+        <input id="editor-firstname" type=text v-model="contact.first_name"  />
+    </div>
 </div>
