25211973e4f29ab9a0cb133942c06c6cb71099f4
Richard Wortham <richard@somekindofweb.dev>
2022-06-25 08:38:33 +0100

    Add all fields to editor

--- app/Views/contact_book.php
+++ app/Views/contact_book.php
@@ -60,5 +60,19 @@
     <div id=contact-editor>
         <label for="editor-firstname">First name:</label>
         <input id="editor-firstname" type=text v-model="contact.first_name"  />
+        <label for="editor-middlename">Middle name:</label>
+        <input id="editor-middlename" type=text v-model="contact.middle_name"  />
+        <label for="editor-lastname">Last name:</label>
+        <input id="editor-lastname" type=text v-model="contact.last_name"  />
+        <label for="editor-dateofbirth">Date of birth:</label>
+        <input id="editor-dateofbirth" type=text v-model="contact.date_of_birth"  />
+        <label for="editor-address">Address:</label>
+        <input id="editor-address" type=text v-model="contact.address"  />
+        <label for="editor-postcode">Postcode:</label>
+        <input id="editor-postcode" type=text v-model="contact.postcode"  />
+        <label for="editor-telephone_number">Telephone number:</label>
+        <input id="editor-telephonenumber" type=text v-model="contact.telephone_number"  />
+        <label for="editor-email">Email:</label>
+        <input id="editor-email" type=text v-model="contact.email"  />
     </div>
 </div>
